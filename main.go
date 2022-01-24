c0af9bedbbcd53799c7afaf084afd1dc8121ee4e
M S Vishwanath Bhat <msvbhat@gmail.com>
2022-01-24 13:54:31 +0100

    Get service name and annotation from endpoint name
    Signed-off-by: M S Vishwanath Bhat <msvbhat@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
@@ -38,4 +40,22 @@ func onEndpointAdd(obj interface{}) {
 	// casting obj as Endpoints
 	ep := obj.(*v1.Endpoints)
 	log.Printf("New Endpoint added: %s", ep.Name)
+
+	kubeconfig := os.Getenv("KUBECONFIG")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	svc, err := clientset.CoreV1().Services(ep.Namespace).Get(context.TODO(), ep.Name, metav1.GetOptions{})
+	if err != nil {
+		log.Printf("Unable to find the service: %s", ep.Name)
+	} else {
+		log.Printf("The service found: %s and annotations are: %s", svc.Name, svc.Annotations)
+	}
+
 }
