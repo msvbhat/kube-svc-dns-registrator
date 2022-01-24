317800519532379895461939615b9c3c25e0977e
M S Vishwanath Bhat <msvbhat@gmail.com>
2022-01-24 14:53:23 +0100

    Store kubernetes clientiset in a global variable
    Also check if the service exist for the endpoint exclusively.
    
    
    Signed-off-by: M S Vishwanath Bhat <msvbhat@gmail.com>

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+var clientset *kubernetes.Clientset
 
 func main() {
 	kubeconfig := os.Getenv("KUBECONFIG")
@@ -20,7 +23,7 @@ func main() {
 	if err != nil {
 		log.Panic(err.Error())
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset, err = kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Panic(err.Error())
 	}
@@ -41,21 +44,15 @@ func onEndpointAdd(obj interface{}) {
 	ep := obj.(*v1.Endpoints)
 	log.Printf("New Endpoint added: %s", ep.Name)
 
-	kubeconfig := os.Getenv("KUBECONFIG")
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Panic(err.Error())
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Panic(err.Error())
-	}
-
 	svc, err := clientset.CoreV1().Services(ep.Namespace).Get(context.TODO(), ep.Name, metav1.GetOptions{})
 	if err != nil {
-		log.Printf("Unable to find the service: %s", ep.Name)
-	} else {
-		log.Printf("The service found: %s and annotations are: %s", svc.Name, svc.Annotations)
+		if errors.IsNotFound(err) {
+			log.Printf("Service %s is not found in namespace %s", ep.Name, ep.Namespace)
+			return
+		} else {
+			log.Printf("Error finding service %s", ep.Name)
+			return
+		}
 	}
-
+	log.Printf("The service found: %s and annotations are: %s", svc.Name, svc.Annotations)
 }
